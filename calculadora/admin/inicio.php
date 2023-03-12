0a2ef24e05308f8bf72fea471d1dce602b602bcb
javier <rebel_jaz@hotmail.com>
2023-03-12 16:43:40 +0100

    Creación estructura plugin

---
+++ calculadora/admin/inicio.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Carga el formulario en el panel de administración de WordPress
+ */
+function calc_dashboard()
+{	
+		
+?>
+
+<h2>Hola Mundo</h2>
+
+<?php
+}
+?>
---
+++ calculadora/calculadora.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Calculadora API
+Plugin URI: https://www.linkedin.com/in/javier-ramos/
+Description: Plugin para enviar datos de una calculadora a una API
+Version: 1.0
+Author: Javier Ramos
+Author URI: https://www.linkedin.com/in/javier-ramos/
+License: GPLv2
+*/
+
+
+defined('ABSPATH') or die("Hasta luego");
+
+
+require_once( plugin_dir_path(__FILE__) . 'admin/inicio.php');
+
+wp_enqueue_style( 'bootstrap', plugin_dir_url( __FILE__ ) . 'css/bootstrap.css' );
+
+wp_enqueue_script( 'jquery', plugin_dir_url( __FILE__ ) . 'js/jquery-3.6.4.min.js', array( 'jquery' ), '3.6', true );
+
+
+
+add_action('admin_menu', 'calc_menu_admin');
+
+function calc_menu_admin() {
+
+
+    add_menu_page('Mi Plugin', 'Calculadora', 'administrator', '/calculadora', 'calc_dashboard');
+}
